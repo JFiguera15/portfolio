ead92a700fefca511518ae5a0d5289d21985b90c
Juan Figuera <juanantoniofiguera@gmail.com>
2024-08-18 12:01:31 +0200

    fix: fixed navlinks color changing

--- src/app/[locale]/components/NavLink.tsx
+++ src/app/[locale]/components/NavLink.tsx
@@ -5,15 +5,15 @@ import Link from 'next/link'
 import { useTranslation } from 'react-i18next';
 
 export default function NavLinks() {
-    const pathname = usePathname().split("/")[2];
+    const pathname = usePathname().split("/");
     const { t } = useTranslation();
 
     return (
         <>
-            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname ? '' : 'text-primary'}`} href="/">{t("aboutMe")}</Link></li>
-            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname === 'projects' ? 'text-primary' : ''}`} href="/projects">{t("projects")}</Link></li>
-            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname === 'skills' ? 'text-primary' : ''}`} href="/skills">{t("skills")}</Link></li>
-            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname === 'contact' ? 'text-primary' : ''}`} href="/contact">{t("contactMe")}</Link></li>
+            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname[2] ? '' : 'text-primary'}`} href="/">{t("aboutMe")}</Link></li>
+            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname.some((e) => e === 'projects')  ? 'text-primary' : ''}`} href="/projects">{t("projects")}</Link></li>
+            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname.some((e) => e === 'skills') ? 'text-primary' : ''}`} href="/skills">{t("skills")}</Link></li>
+            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname.some((e) => e === 'contact') ? 'text-primary' : ''}`} href="/contact">{t("contactMe")}</Link></li>
         </>
     )
 }
