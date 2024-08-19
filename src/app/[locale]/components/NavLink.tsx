18ab1213e9d08d82f7e80c31785a45598e2ae651
Juan Figuera <juanantoniofiguera@gmail.com>
2024-08-19 11:02:55 +0200

    fix: fixed the fixing of the fixing

--- src/app/[locale]/components/NavLink.tsx
+++ src/app/[locale]/components/NavLink.tsx
@@ -6,11 +6,12 @@ import { useTranslation } from 'react-i18next';
 
 export default function NavLinks() {
     const pathname = usePathname().split("/");
+    const fullPath = usePathname();
     const { t } = useTranslation();
 
     return (
         <>
-            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${usePathname() === '/' || usePathname() === '/es' ? 'text-primary' : ''}`} href="/">{t("aboutMe")}</Link></li>
+            <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${fullPath === '/' || fullPath === '/es' ? 'text-primary' : ''}`} href="/">{t("aboutMe")}</Link></li>
             <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname.some((e) => e === 'projects')  ? 'text-primary' : ''}`} href="/projects">{t("projects")}</Link></li>
             <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname.some((e) => e === 'skills') ? 'text-primary' : ''}`} href="/skills">{t("skills")}</Link></li>
             <li className="drop-shadow-xl my-auto md:transition md:ease-in-out md:hover:-translate-y-2"><Link className={`link ${pathname.some((e) => e === 'contact') ? 'text-primary' : ''}`} href="/contact">{t("contactMe")}</Link></li>
